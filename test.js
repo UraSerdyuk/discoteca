0dcd82c9833f36b56b8b8cd11d77a24193aa2deb
Yura <cb8593bc@gmail.com>
2019-10-07 15:27:06 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,7 @@
 let container = document.querySelector('#container');
 let div = document.createElement('div');
 container.append(div);
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-setInterval(()=>{
-    setInterval(()=>{
-        div.style.backgroundColor=`rgb(${getRandomInt(256)}, ${getRandomInt(256)},${getRandomInt(256)} )`;
-    },getRandomInt(1000));
-},3000);
+function getRandomInt(max) { return Math.floor(Math.random() * Math.floor(max));}
+setInterval(()=>{setInterval(()=>{  div.style.backgroundColor=`rgb(${getRandomInt(256)}, ${getRandomInt(256)},${getRandomInt(256)} )`; },getRandomInt(1000));},3000);
 div.style.width='100%';
 div.style.height='100vh';
